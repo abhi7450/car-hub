68692c2275512cfb3463ac4e5ae878c3af23c1c7
A3HI <abhishekashok64@gmail.com>
2023-06-12 10:38:54 +0530

    Code till date

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./section/Footer";
+import NavBar from "./section/Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./Searchbar";
+import Hero from "./section/Hero";
+
+export { Hero, CarCard, CustomButton, CustomFilter, Footer, NavBar, ShowMore, SearchBar };
